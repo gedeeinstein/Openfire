df6f0c72c9d212947f48f1ae360559fd5d6029c5
Dan Caseley <dan@caseley.me.uk>
2025-03-22 20:31:57 +0000

    Use modern bash subshells on macOS

--- distribution/src/bin/extra/openfire-launchd-wrapper.sh
+++ distribution/src/bin/extra/openfire-launchd-wrapper.sh
@@ -6,7 +6,7 @@ function shutdown()
 {
 	date
 	echo "Shutting down Openfire"
-    kill -s TERM `ps auxww | grep -v wrapper | awk '/openfire/ && !/awk/ {print $2}'`
+    kill -s TERM $(ps auxww | grep -v wrapper | awk '/openfire/ && !/awk/ {print $2}')
 }
 
 date
@@ -14,7 +14,7 @@ echo "Starting Openfire"
 
 /usr/bin/java -server -jar "$OPENFIRE_HOME/lib/startup.jar" -Dlog4j.configurationFile=$$OPENFIRE_HOME/lib/log4j2.xml -Dlog4j2.formatMsgNoLookups=true -Dopenfire.lib.dir=/usr/local/openfire/lib&
 
-OPENFIRE_PID=`ps auxww | grep -v wrapper | awk '/openfire/ && !/awk/ {print $2}'`
+OPENFIRE_PID=$(ps auxww | grep -v wrapper | awk '/openfire/ && !/awk/ {print $2}')
 
 # allow any signal which would kill a process to stop Openfire
 trap shutdown HUP INT QUIT ABRT KILL ALRM TERM TSTP
