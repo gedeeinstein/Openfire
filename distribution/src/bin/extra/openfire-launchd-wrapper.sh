7d04363e348c5c7af3a69b97c2b9ee057881c928
Dan Caseley <dan@caseley.me.uk>
2025-03-22 20:32:44 +0000

    Match macOS launchd vars to standard openfire.sh

--- distribution/src/bin/extra/openfire-launchd-wrapper.sh
+++ distribution/src/bin/extra/openfire-launchd-wrapper.sh
@@ -12,7 +12,22 @@ function shutdown()
 date
 echo "Starting Openfire"
 
-/usr/bin/java -server -jar "$OPENFIRE_HOME/lib/startup.jar" -Dlog4j.configurationFile=$$OPENFIRE_HOME/lib/log4j2.xml -Dlog4j2.formatMsgNoLookups=true -Dopenfire.lib.dir=/usr/local/openfire/lib&
+OPENFIRE_OPTS=""
+OPENFIRE_OPTS="$OPENFIRE_OPTS -DopenfireHome=$OPENFIRE_HOME"
+OPENFIRE_OPTS="$OPENFIRE_OPTS -Dopenfire.lib.dir=$OPENFIRE_HOME/lib"
+OPENFIRE_OPTS="$OPENFIRE_OPTS -Djava.security.properties=$OPENFIRE_HOME/resources/security/java.security"
+OPENFIRE_OPTS="$OPENFIRE_OPTS -Djdk.tls.server.enableStatusRequestExtension=true"
+OPENFIRE_OPTS="$OPENFIRE_OPTS -Dcom.sun.security.enableCRLDP=true"
+
+
+JAVA_CMD_OPTS=""
+JAVA_CMD_OPTS="$JAVA_CMD_OPTS -Dlog4j.configurationFile=$OPENFIRE_HOME/lib/log4j2.xml"
+JAVA_CMD_OPTS="$JAVA_CMD_OPTS -Dlog4j2.formatMsgNoLookups=true"
+JAVA_CMD_OPTS="$JAVA_CMD_OPTS -Djdk.tls.ephemeralDHKeySize=matched"
+JAVA_CMD_OPTS="$JAVA_CMD_OPTS -Djsse.SSLEngine.acceptLargeFragments=true"
+JAVA_CMD_OPTS="$JAVA_CMD_OPTS -Djava.net.preferIPv6Addresses=system"
+
+/usr/bin/java $JAVA_CMD_OPTS -server $OPENFIRE_OPTS -jar "$OPENFIRE_HOME/lib/startup.jar" &
 
 OPENFIRE_PID=$(ps auxww | grep -v wrapper | awk '/openfire/ && !/awk/ {print $2}')
 
